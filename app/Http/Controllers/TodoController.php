f4ed54ef60d68d019d71f8466b6f5a2a84d76027
Enam <surelastmail@gmail.com>
2020-07-28 09:42:13 +0600

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Todo;
+
+class TodoController extends Controller
+{
+ public function getAllTodo(Request $request){
+
+    Auth()->
+     return  Todo->find
+ }
+}
---
+++ app/models/TodoModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'todo';
+}
